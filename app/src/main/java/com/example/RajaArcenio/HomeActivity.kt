d5c7db143f7c5123817fcae1dc1e70b51d8de729
Raja Arcenio <ravihussain49@gmail.com>
2026-09-21 22:12:11 +0700

    Praktikum Pertemuan 3:List

---
+++ app/src/main/java/com/example/RajaArcenio/HomeActivity.kt
@@ -0,0 +1,40 @@
+package com.example.RajaArcenio
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.RajaArcenio.ui.theme.JualanTheme
+import com.example.RajaArcenio.ui.screen.DaftarProdukScreen
+
+class HomeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JualanTheme {
+                DaftarProdukScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting2(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview2() {
+    JualanTheme {
+        Greeting2("Android")
+    }
+}
